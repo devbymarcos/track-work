0a36ee14e02983ac2516ffe296663b774d709010
devbymarcos <marcoslopes.dev@gmail.com>
2023-12-15 14:03:18 -0300

    first commit

--- app.py
+++ app.py
@@ -1,28 +1,72 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import  sleep
+from time import sleep
 from selenium.webdriver.common.keys import Keys
+from openpyxl import Workbook
+
+print("vamos começar a buscar suas vagas")
+search = input("digite sua busca: ")
+login_email = input("digite seu login: ")
+login_password = input("insira sua senha: ")
+# inicia o navegador
+
 
 browser = webdriver.Chrome()
 browser.get("https://www.linkedin.com/")
-
 sleep(2)
-email = browser.find_element(By.XPATH,"//input[@id='session_key']")
-password = browser.find_element(By.XPATH,"//input[@id='session_password']")
-btn_entrar = browser.find_element(By.XPATH,"//button[normalize-space(text())='Entrar']")
+email = browser.find_element(By.XPATH, "//input[@id='session_key']")
+password = browser.find_element(By.XPATH, "//input[@id='session_password']")
+btn_entrar = browser.find_element(By.XPATH, "//button[normalize-space(text())='Entrar']")
 sleep(2)
-email.send_keys("marcoslopes.cwb@gmail.com")
+email.send_keys(login_email)
 sleep(2)
-password.send_keys("sepol7840")
+password.send_keys(login_password)
 sleep(2)
 btn_entrar.click()
 sleep(5)
 browser.get("https://www.linkedin.com/jobs/")
 sleep(5)
-input_jobs_search = browser.find_element(By.XPATH,"//input[@id='jobs-search-box-keyword-id-ember23']")
+input_jobs_search = browser.find_element(By.XPATH, "//header//input")
 sleep(5)
-input_jobs_search.send_keys("Desenvolvedor python")
+input_jobs_search.send_keys(search)
 sleep(5)
 input_jobs_search.send_keys(Keys.ENTER)
 sleep(5)
-sleep(20)
+ul_element = browser.find_element(By.CSS_SELECTOR, "main div.jobs-search-results-list")
+sleep(5)
+
+
+def scroll_list(pixels):
+    browser.execute_script(f"arguments[0].scrollTop += {pixels};", ul_element)
+    sleep(2)
+
+
+links = []
+for _ in range(25):
+    scroll_list(200)
+    links = browser.find_elements(By.XPATH, "//main//div/div//ul//li//a[@data-control-id]")
+    print(len(links))
+    if len(links) >= 25:
+        print(f'chegamos ao numero esperado de {len(links)}')
+        break
+
+spreadsheet = Workbook()
+
+sheet = spreadsheet.active
+
+sheet['A1'] = "NOME DA VAGA"
+sheet['B1'] = "LINK DA VAGA"
+next_line = sheet.max_row + 1
+
+for link in links:
+    text = link.text
+    url_link = link.get_attribute("href")
+
+    sheet[f'A{next_line}'] = text
+    sheet[f'B{next_line}'] = url_link
+
+    next_line += 1
+
+spreadsheet.save("vagas_links.xlsx")
+print("planilha criada")
+
